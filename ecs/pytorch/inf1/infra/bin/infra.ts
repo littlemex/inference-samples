eee692c0c8774f1a452f2e03b7044a3528247f4a
Ubuntu <ubuntu@ip-172-31-127-57.ec2.internal>
2022-04-04 15:29:25 +0000

    modify env

--- ecs/pytorch/inf1/infra/bin/infra.ts
+++ ecs/pytorch/inf1/infra/bin/infra.ts
@@ -11,14 +11,13 @@ new InfraStack(app, 'InfraStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  /* env: { 
+  env: { 
     account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
-  }, */
+  },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '067150986393', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
