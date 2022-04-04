61cf24addf02ccafb9e27f8ee0988f9d4622a3cc
Ubuntu <ubuntu@ip-172-31-127-57.ec2.internal>
2022-04-04 15:34:28 +0000

    rm --chached d.ts, js

--- ecs/pytorch/inf1/infra/lib/infra-stack.d.ts
+++
@@ -1,4 +0,0 @@
-import cdk = require('aws-cdk-lib');
-export declare class InfraStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps);
-}
--- ecs/pytorch/inf1/infra/lib/resources/ecs-cluster.d.ts
+++
@@ -1,4 +0,0 @@
-import cdk = require('aws-cdk-lib');
-export declare class ECSCluster extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps);
-}
