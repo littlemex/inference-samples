a7279758f7f0de4e71b47de926a4a0023068e292
Ubuntu <ubuntu@ip-172-31-5-59.ap-northeast-1.compute.internal>
2022-05-31 12:04:47 +0000

    typo

--- cdk/ec2/src/config.sh
+++ cdk/ec2/src/config.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -x
 
-HOME=/home/ubuntu
-
-cd $HOME
-
 # Please see the neuron docs
 #   https://awsdocs-neuron.readthedocs-hosted.com/en/latest/neuron-intro/pytorch-setup/pytorch-install.html
 sudo apt-get update -y
@@ -14,11 +10,21 @@ sudo apt-get install aws-neuron-tools -y
 export PATH=/opt/aws/neuron/bin:$PATH
 
 sudo apt-get install -y python3.7-venv g++
-python3.7 -m venv pytorch_venv
-source pytorch_venv/bin/activate
-pip install -U pip
-pip install ipykernel 
-pip install jupyter notebook
-pip install environment_kernels
-pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
-pip install torch-neuron==1.10.2.* neuron-cc[tensorflow] "protobuf<4" torchvision
+
+cd /home/ubuntu
+sudo -u ubuntu python3.7 -m venv pytorch_venv
+
+cat <<'EOL' > pip.sh
+#!/bin/sh
+export PIP='./pytorch_venv/bin/pip3'
+echo $PIP
+$PIP install -U pip
+$PIP install ipykernel
+$PIP install jupyter notebook
+$PIP install environment_kernels
+$PIP config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
+$PIP install torch-neuron==1.10.2.* neuron-cc[tensorflow] "protobuf<4" torchvision
+$PIP install transformers==4.19.2 fugashi==1.1.2 ipadic==1.0.0
+EOL
+chmod +x pip.sh
+sudo -u ubuntu ./pip.sh
