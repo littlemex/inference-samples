719601a87926ef676f1d69bbc22eb9a45d8ef46e
Ubuntu <ubuntu@ip-172-31-5-59.ap-northeast-1.compute.internal>
2022-06-02 04:41:50 +0000

    rmv getcontext region

--- cdk/ec2/bin/ec2.ts
+++ cdk/ec2/bin/ec2.ts
@@ -4,7 +4,6 @@ import { Ec2Stack } from '../lib/ec2-stack';
 
 
 const app = new cdk.App();
-const region: string = app.node.tryGetContext("region");
 new Ec2Stack(app, 'Ec2Stack', {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
