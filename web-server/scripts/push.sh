c260d6dfe7a83072735830aaab821c210ebda04f
Ubuntu <ubuntu@ip-172-31-5-59.ap-northeast-1.compute.internal>
2022-06-03 16:21:00 +0000

    init web server

---
+++ web-server/scripts/push.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+IMAGE_NAME=web
+TAG=':latest'
+repoarn=$(aws ecr describe-repositories \
+  --query "repositories[?repositoryName==\`${IMAGE_NAME}\`].[repositoryArn][]|[0]" \
+  --output text)
+region=$(echo $repoarn |sed "s/.*:ecr:\(.*\):\(.*\):repo.*/\1/")
+accont=$(echo $repoarn |sed "s/.*:ecr:\(.*\):\(.*\):repo.*/\2/")
+registory=${accont}.dkr.ecr.ap-northeast-1.amazonaws.com
+image=${registory}/${IMAGE_NAME}${TAG}
+
+aws ecr get-login-password --region $region \
+  | docker login --username AWS --password-stdin $registory
+
+docker tag ${IMAGE_NAME}:latest $image
+
+docker push $image
