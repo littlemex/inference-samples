9d4d988fbc6f4b054df1c5f6d9f09cea488c0879
Ubuntu <ubuntu@ip-172-31-5-59.ap-northeast-1.compute.internal>
2022-06-24 19:51:15 +0000

    rm add-neuron-repo.sh

--- model-server/build/add-neuron-repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-tee /etc/yum.repos.d/neuron.repo > /dev/null <<EOF
-[neuron]
-name=Neuron YUM Repository
-baseurl=https://yum.repos.neuron.amazonaws.com
-enabled=1
-metadata_expire=0
-EOF
