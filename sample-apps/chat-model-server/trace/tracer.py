f750f8f12f9b8d5acd4b788270f31cc31f3619c0
Ubuntu <ubuntu@ip-172-31-46-242.ec2.internal>
2023-06-14 08:11:18 +0000

    analyze

--- sample-apps/chat-model-server/trace/tracer.py
+++ sample-apps/chat-model-server/trace/tracer.py
@@ -1,4 +1,5 @@
 import re
+import json
 import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
@@ -7,9 +8,30 @@ os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 instance_type = re.sub(r'[^a-zA-Z0-9]', '', os.environ['INSTANCE_TYPE'])
 print(instance_type)
 
+def get_torch_jit(instance_type, model, inputs_tuple):
+    print(f"{instance_type}, analyze start")
+    if instance_type == 'inf1':
+        import torch_neuron
+        torch_jit = torch.neuron
+        try:
+            torch_jit.analyze_model(model, inputs_tuple)
+        except:
+            pass
+    elif instance_type == 'inf2' or instance_type == 'trn1':
+        import torch_neuronx
+        torch_jit =torch_neuronx
+        try:
+            torch_jit.analyze(model, inputs_tuple)
+        except:
+            pass
+    else:
+        torch_jit = torch.jit
+        print("nothing to do.")
+    print(f"{instance_type}, analyze end")
+    return torch_jit
+
 model_name = "ybelkada/japanese-roberta-question-answering"
 ptname = "transformers_neuron.pt"
-processor = "inf1"
 LENGTH = 512
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -22,22 +44,9 @@ context = "My name is Clara and I live in Berkeley."
 
 inputs = tokenizer(
     question, context, add_special_tokens=True, return_tensors="pt", max_length=LENGTH, padding='max_length', truncation=True)
-
-
 inputs_tuple = (inputs['input_ids'], inputs['attention_mask'])
 
-if instance_type == 'inf1':
-    import torch_neuron
-    torch_jit = torch.neuron
-    print("inf1, analyze start")
-    torch_jit.analyze_model(model, inputs_tuple)
-    print("inf1, analyze end")
-elif instance_type == 'inf2' or instance_type == 'trn1':
-    import torch_neuronx
-    torch_jit = torch_neuronx
-else:
-    torch_jit = torch.jit
-
+torch_jit = get_torch_jit(instance_type, model, inputs_tuple)
 model_traced = torch_jit.trace(model, inputs_tuple)
 model_traced.save(ptname)
 
